e11be80ecaa74370b07a42d888efa59e2fb5e342
wangyifan <wangyifan@myhexin.com>
2019-11-01 15:53:05 +0800

    日期选择器默认设置

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# element_example
-饿了么 框架 示例
+# 饿了么 框架 示例
+# 1、vue-cli@3 中引入 element框架
+  # 介绍： github.com/ElementUI/vue-cli-plugin-element
+  # 安装 sass
+    npm install node-sass --save-dev
+    npm install sass-loader --save-dev
+
+# 2、示例
+  # el-date-picker： 默认日期设置
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="example">
+      <div class="module-item">
+        <span style="margin-right: 40px;">日期选择器:</span>
+        <el-date-picker 
+          v-model="dateVals" 
+          type="daterange" 
+          value-format="yyyy-MM-dd"
+          range-separator="-" 
+          start-placeholder="开始日期" 
+          end-placeholder="结束日期">
+        </el-date-picker>
+      </div>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -10,19 +22,41 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      dateVals: ''
+    }
+  },
+  methods: {
+    defaultDate(){ //设置默认日期
+      let date = new Date();
+      let year = date.getFullYear().toString();
+      let month = date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1).toString():(date.getMonth()+1).toString();
+      let da = date.getDate() < 10 ? '0'+date.getDate().toString():date.getDate().toString();
+      let beg = year + '-' + month + '-01';
+      let end = year + '-' + month + '-' + da;
+      this.dateVals = [beg,end];
+    }
+  },
   components: {
     HelloWorld
+  },
+  created() {
+    this.defaultDate();
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .example{
+    display: flex;
+  }
 </style>
